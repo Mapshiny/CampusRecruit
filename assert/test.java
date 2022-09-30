8462525d5cf851d68d735b87a88efd7a928753df
Chrix Wu <820499122@qq.com>
2022-09-30 20:09:04 +0800

    Update Assert

---
+++ assert/test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class test {
+    public static void main(String[] args) {
+        int i = 0;
+        assert i == 0;
+        System.out.println("i = " + i);
+    }
+}
